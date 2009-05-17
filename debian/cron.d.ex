2599c9b53c399201deacf58fdce72e60d66bf5ba
David Bremner <bremner@unb.ca>
2009-05-17 08:54:36 -0300

    remove unused dh_make example files

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the nauty package
-#
-0 4	* * *	root	nauty_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(nauty):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="nauty" command="/usr/bin/nauty"
